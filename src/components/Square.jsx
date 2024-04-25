2096a2ad91a40fc1e91f44050b44bc4099225b16
“MernWithManas” <yesthisismanas@gmail.com>
2024-04-25 18:06:49 +0530

    sass installed and styles.scss file created with some bugs

---
+++ src/components/Square.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Square = ({value}) => {
+  return (
+    <>
+       <button type="button" className = 'square'>
+        {value}
+       </button>
+    </>
+  );
+};
+
+
+export default Square;
